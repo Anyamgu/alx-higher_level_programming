4f470e9cb6e5eda585c0b00983af8cde52eab547
Anyamgu <anyangudelight@gmail.com>
2023-12-06 11:25:45 -0800

    Script Dynamic

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    i = len(argv) - 1
+    if i < 1:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} arguments.".format(i))
+    else:
+        print("{} arguments.".format(i))
+    
+    for i in range(i):
+        print("{}: {:s}".format(i + 1, argv[i + 1]))
